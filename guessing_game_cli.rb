249b78d772e704fbd48f826d2641fe4bcafe3983
Anh Vuong <vuonga1103@yahoo.com>
2020-06-11 13:45:18 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -10,6 +10,6 @@ def run_guessing_game
   elsif user_input == random_number
     puts "You guessed the correct number!"
   else
-    
+    puts "Sorry! The computer guessed #{random_number}."
   end
 end
